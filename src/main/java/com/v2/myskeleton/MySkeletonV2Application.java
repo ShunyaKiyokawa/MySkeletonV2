15cb552f3725fc1afab2d01298591cbee1229768
m0112164e3@edu.teu.ac.jp <m0112164e3@edu.teu.ac.jp>
2017-04-30 15:18:51 +0900

    initial commit spring徹底入門にもとづきこれからつくってみる

---
+++ src/main/java/com/v2/myskeleton/MySkeletonV2Application.java
@@ -0,0 +1,12 @@
+package com.v2.myskeleton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySkeletonV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySkeletonV2Application.class, args);
+	}
+}
